4605822004c770ba75391380790ff3b18e9bfc1a
raphita-dev <raphita.web@gmail.com>
2024-06-08 01:05:28 +0700

    feat: banyak dah

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const routers = require("./src/routers");
+const bodyParser = require("body-parser");
 
 const cors = require("cors");
 const app = express();
 
 const port = 5000;
-
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(bodyParser.json());
 app.use("/api/raphita", routers);
 app.use(express.static("uploads"));
 app.use("/uploads", express.static("uploads"));
